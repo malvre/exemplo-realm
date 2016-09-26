be6a121c146be87723097a7fbbc329bef19fc8c9
Marcelo Rezende <malvre@gmail.com>
2016-09-26 11:28:22 -0300

    Update queries.java

--- queries.java
+++ queries.java
@@ -1,5 +1,16 @@
 Realm realm = Realm.getDefaultInstance();
 
-RealmResults<Person> teenagers = realm.where(Person.class).between("age", 13, 20).findAll();
 
+// exemplo 1
+RealmResults<Person> teenagers = realm.where(Person.class).between("age", 13, 20).findAll();
 Person firstJohn = teenagers.where().equalTo("name", "John").findFirst();
+
+// exemplo 2
+RealmResults<Pessoa> pessoas = realm.where(Pessoa.class).findAll();
+for (Pessoa pessoa : pessoas) {
+	showStatus(String.format(Locale.getDefault(), "%s (%d)", pessoa.getNome(), pessoa.getIdade()));
+	showStatus(String.format("*** %s", pessoa.getCachorro().nome));
+	for (Gato gato : pessoa.getGatos()) {
+		showStatus(String.format("*** *** %s", gato.nome));
+	}
+}
